6c839de62aa148f3207032d27d3fb9a4c8258463
kumar-rakshith <rakshithkumar.thokur@gmail.com>
2024-08-12 21:33:37 +0530

    adding file s

---
+++ src/components/ChannelDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChannelDetails = () => {
+  return (
+    <div>
+         <h2> gandu</h2>
+    </div>
+  )
+}
+
+export default ChannelDetails
---
+++ src/components/VideoDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoDetails = () => {
+  return (
+    <div>
+         <h2> ljgyfv</h2>
+    </div>
+  )
+}
+
+export default VideoDetails
